968c222dc421f7f021d84c4a7b72b779dd50ed63
KapilSinghThakuri <kapilsinghthakuri33@gmail.com>
2024-01-21 21:09:01 +0545

    PrivacyPolicy Page Created

--- resources/views/eBookStore/footerPages/privacyPolicy.blade.php
+++ resources/views/eBookStore/footerPages/privacyPolicy.blade.php
@@ -6,19 +6,55 @@
     <div class="row">
         <div class="col-md-12">
             <div class="privacy-policy m-5">
-                <h5 class="text-primary">Privacy Policy</h5>
-                <p>
-                    This page is used to inform website visitors regarding our policies with the collection, use, and
-                    disclosure of Personal Information if anyone decided to use our Service. <br><br>
+                <div>
+                    <h5 class="text-primary">Privacy Policy</h5>
+                    <p>
+                        This page is used to inform website visitors regarding our policies with the collection, use, and
+                        disclosure of Personal Information if anyone decided to use our Service. <br><br>
 
-                    If you choose to use our Service, then you agree to the collection and use of information in
-                    relation with this policy. The Personal Information that we collect are used for providing and
-                    improving the Service. We will not use or share your information with 
-                    anyone except as described in this Privacy Policy. <br><br>
+                        If you choose to use our Service, then you agree to the collection and use of information in
+                        relation with this policy. The Personal Information that we collect are used for providing and
+                        improving the Service. We will not use or share your information with 
+                        anyone except as described in this Privacy Policy. <br><br>
 
-                    The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions,
-                    which is accessible at , unless otherwise defined in this Privacy Policy.
-                </p>
+                        The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions,
+                        which is accessible at , unless otherwise defined in this Privacy Policy.
+                    </p>
+                </div>
+                <div>
+                    <h5>Information Collection and Use</h5>
+                    <p>
+                        For a better experience while using our Service, we may require you to provide us with certain
+                        personally identifiable information, including but not limited to your name, phone number, and
+                        postal address. The information that we collect will be used to contact or identify you.
+                    </p>
+                </div>
+                <div>
+                    <h5>Log Data</h5>
+                    <p>
+                        We want to inform you that whenever you visit our Service, we collect information that your 
+                        browser sends to us that is called Log Data. This Log Data may include information such as your 
+                        computer's Internet Protocol address, browser version, pages of our Service that you visit, 
+                        the time and date of your visit, the time spent on those pages, and other statistics.
+                    </p>
+                </div>
+                <div>
+                    <h5>Cookies</h5>
+                    <p>
+                        Cookies are files with small amount of data that is commonly used an anonymous unique identifier. 
+                        These are sent to your browser from the website that you visit and are stored on your computer'™s 
+                        hard drive. <br><br>
+
+                        Our website uses these cookies to collection information and to improve our Service.
+                        You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer.
+                        If you choose to refuse our cookies, you may not be able to use some portions of our Service.
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        
+                    </p>
+                </div>
             </div>
         </div>
     </div>
