2296ff530521b7bd74d7d50774b14c00061ef266
KapilSinghThakuri <kapilsinghthakuri33@gmail.com>
2024-02-04 23:34:55 +0545

    help Category Added

--- resources/views/eBookStore/topbarPages/help.blade.php
+++ resources/views/eBookStore/topbarPages/help.blade.php
@@ -10,7 +10,12 @@
             <h3 class="text-primary text-center">Help</h3>
 
             <div class="m-5">
-                <h1><h1>
+                <h1>Choose needed help<h1>
+                <ul>                                            
+                    <li><a href="">Account Related</a></li>
+                    <li><a href="">Privacy & Policy Related</a></li>
+                    <li><a href="">Transaction Related</a></li>
+                </ul>
             </div>
         </div>
     </div>
