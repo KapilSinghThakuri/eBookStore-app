0b34aca6e56169d5d76bb1b57ade0cadc8c40f94
KapilSinghThakuri <kapilsinghthakuri33@gmail.com>
2024-02-12 19:57:14 +0545

    adjust features

--- resources/views/eBookStore/signin.blade.php
+++ resources/views/eBookStore/signin.blade.php
@@ -11,7 +11,7 @@
             <form action="">
                 <h1>Login</h1>
                 <div class="input-box">
-                    <input type="text" placeholder="Username" required>
+                    <input type="text" placeholder="Username or " required>
                     <i class='bx bxs-user'></i>
                 </div>
                 <div class="input-box">
