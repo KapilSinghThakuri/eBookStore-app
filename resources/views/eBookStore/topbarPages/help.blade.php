72b47a11bb8eb887671467c4c4a7678ca4e2071e
KapilSinghThakuri <kapilsinghthakuri33@gmail.com>
2024-02-23 14:11:37 +0545

    help desk created

--- resources/views/eBookStore/topbarPages/help.blade.php
+++ resources/views/eBookStore/topbarPages/help.blade.php
@@ -7,14 +7,35 @@
     <div class="row">
         <div class="col-md-12">
 
-            <h3 class="text-primary text-center">Help</h3>
+            <h3 class="text-primary text-center">Help Center</h3>
 
-            <div class="m-5">
-                <h1>Choose needed help<h1>
-                <ul>                                            
-                    <li><a href="">Account Related</a></li>
-                    <li><a href="">Privacy & Policy Related</a></li>
-                    <li><a href="">Transaction Related</a></li>
+            <div class="m-4" >
+                <ul class="d-flex justify-content-center" style="list-style: none;">
+                    <li>
+                        <div class="card m-2" style="border-radius: 10px;">
+                            <div class="card-body"> <a href="">Account Related</a> </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card m-2" style="border-radius: 10px;">
+                            <div class="card-body"> <a href="">Privacy & Policy Related</a> </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card m-2" style="border-radius: 10px;">
+                            <div class="card-body"> <a href="">Transaction Related</a> </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card m-2" style="border-radius: 10px;">
+                            <div class="card-body"> <a href="">Transaction Related</a> </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card m-2" style="border-radius: 10px;">
+                            <div class="card-body"> <a href="">Transaction Related</a> </div>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
